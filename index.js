fb2c39d8b0bb03f884abf323ddfb55aca4e3d97a
Julian Gruber <julian@juliangruber.com>
2013-08-03 22:07:09 -0400

    install level-live-stream on db

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var live = require('level-live-stream');
+var liveStream = require('level-live-stream');
 var through = require('through');
 var reactive = require('reactive-component');
 
 module.exports = function (db, el, obj, options) {
+  if (!db.createLiveStream) liveStream.install(db);
+
   var view = reactive(el, obj, options);
 
   view.bind('db-key', function (el, name) {
-    live(db, { start: name, end: name, keys: false })
+    db.createLiveStream({ start: name, end: name, keys: false })
       .pipe(through(function (value) {
         if (value.type == 'put') value = value.value;
         el.innerHTML = '';
@@ -19,7 +21,7 @@ module.exports = function (db, el, obj, options) {
     var container = el.parentNode;
     container.removeChild(el);
 
-    live(db, { start: group + '!', end: group + '!~', keys: false })
+    db.createLiveStream({ start: group + '!', end: group + '!~', keys: false })
       .pipe(through(function (value) {
         if (value.type == 'put') value = value.value;
         var clone = el.cloneNode(true);
